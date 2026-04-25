ced859030760f864522c41696d13c3349c844aec
oguzhaneksi <oguzhan.eksi97@gmail.com>
2026-04-25 13:34:37 +0400

    feat: add GitHub Actions workflow for publishing SDK to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.jetbrains.dokka) apply false
 }
--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.jetbrains.dokka)
 }
 
 android {
@@ -50,4 +52,44 @@ dependencies {
     testImplementation(libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.oguzhaneksi",
+        artifactId = "streamprobe",
+        version = project.property("VERSION_NAME") as String
+    )
+
+    pom {
+        name.set("StreamProbe")
+        description.set("A debug SDK for Android apps that inspects HLS and DASH streaming traffic in real time on top of Media3/ExoPlayer.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/oguzhaneksi/StreamProbe")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("oguzhaneksi")
+                name.set("Oğuzhan Ekşi")
+                url.set("https://github.com/oguzhaneksi")
+            }
+        }
+        scm {
+            url.set("https://github.com/oguzhaneksi/StreamProbe")
+            connection.set("scm:git:https://github.com/oguzhaneksi/StreamProbe.git")
+            developerConnection.set("scm:git:ssh://git@github.com/oguzhaneksi/StreamProbe.git")
+        }
+    }
+
+    // Configure publishing to Maven Central Portal
+    publishToMavenCentral()
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
