743579d0ff83845f9fef4599a697134101fa7560
oguzhaneksi <oguzhan.eksi97@gmail.com>
2026-04-29 15:06:03 +0400

    fix: correct typo in material icons dependency in build.gradle.kts and libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.aandroidx.compose.material.icons)
+    implementation(libs.androidx.compose.material.icons)
 
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.exoplayer.dash)
